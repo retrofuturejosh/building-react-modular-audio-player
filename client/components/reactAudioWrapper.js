89f7616d895c5f5ee80513010c853eed48a53639
Joshua Sohn <joshuadsohn@gmail.com>
2018-02-17 22:27:36 -0500

    creates server, public, and client folders for simple single page application

---
+++ client/components/reactAudioWrapper.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export class ReactAudioWrapper extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="audio-wrapper">
+      Hi!
+      </div>
+    )
+  }
+}
---
+++ client/index.js
@@ -0,0 +1,9 @@
+import './index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ReactAudioWrapper } from './components/reactAudioWrapper'
+
+ReactDOM.render(
+    <ReactAudioWrapper />,
+  document.getElementById('app')
+)
