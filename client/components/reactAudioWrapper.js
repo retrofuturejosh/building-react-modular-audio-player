e7b097c46d300c043e5afc2529dceacd7eae5854
Joshua Sohn <joshuadsohn@gmail.com>
2018-02-22 21:06:57 -0500

    adds comments to volume functions

--- client/components/reactAudioWrapper.js
+++ client/components/reactAudioWrapper.js
@@ -57,23 +57,28 @@ export class ReactAudioWrapper extends Component {
   }
 
   handleVolume(event, muting) {
+    //when handleVolume is called from range onChange
     if (event) {
       //for pure volume sliding
       let newVolume = (event.target.value < 1) ? 0 : event.target.value
       this.setState({volume: newVolume});
       this.audioRef.volume = newVolume / 100;
 
+      //set state.mute to true if sliding to less than 1
       if (newVolume < 1) {
         this.setState({muted: true});
       } else if (this.state.muted) {
+      //set state.mute to false if sliding up input from  mute
         this.setState({muted: false})
       }
+
+    //when calling function from handleMute()
     } else if (muting) {
-      //moving volume slider to 0 and muting
+      //when muting, move volume slider to 0 and set volume to 0
       this.setState({volumePreMute: this.state.volume, volume: "0"})
       this.audioRef.volume = 0;
     } else  {
-      //returning volume and slider to previous position before mute
+      //when unmuting, return volume and slider to previous position before mute
       this.setState({volume: this.state.volumePreMute})
       this.audioRef.volume = this.state.volumePreMute / 100;
     }
