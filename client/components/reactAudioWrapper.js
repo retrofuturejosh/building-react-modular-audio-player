c726b229566489a8129e559aea77187d8b481c72
Joshua Sohn <joshuadsohn@gmail.com>
2018-02-22 20:35:05 -0500

    fixes interval re-assignment bug when pausing

--- client/components/reactAudioWrapper.js
+++ client/components/reactAudioWrapper.js
@@ -6,20 +6,26 @@ export class ReactAudioWrapper extends Component {
     this.state = {
       seekerVal: "0",
       volume: "75",
-      playing: false
+      playing: false,
+      muted: false,
+      volumePreMute: "75",
     };
     this.seekingInterval = null;
     this.handlePlay = this.handlePlay.bind(this);
     this.handlePause = this.handlePause.bind(this);
     this.handleSeekSlider = this.handleSeekSlider.bind(this);
     this.handleSeek = this.handleSeek.bind(this);
-    this.handleSeekDown = this.handleSeekDown.bind(this);
     this.handleVolume = this.handleVolume.bind(this);
-    this.handleProgress = this.handleProgress.bind(this)
+    this.handleProgress = this.handleProgress.bind(this);
+    this.handleMute = this.handleMute.bind(this);
   }
 
   handleProgress() {
+    if (this.seekingInterval) {
+      clearInterval(this.seekingInterval);
+    }
     this.seekingInterval = setInterval( () => {
+      console.log('called');
       let currentAudioTime = (this.audioRef.currentTime / this.audioRef.duration) * 100;
       this.setState({seekerVal: currentAudioTime});
     }, 500);
@@ -35,12 +41,13 @@ export class ReactAudioWrapper extends Component {
   }
 
   handlePause() {
+    clearInterval(this.seekingInterval);
     this.audioRef.pause();
     this.setState({playing: false})
-    clearInterval(this.seekingInterval);
   }
 
   handleSeekSlider(event) {
+    clearInterval(this.seekingInterval);
     this.setState({seekerVal: event.target.value});
   }
 
@@ -50,13 +57,28 @@ export class ReactAudioWrapper extends Component {
     this.handleProgress();
   }
 
-  handleVolume(event) {
-    this.setState({volume: event.target.value});
-    this.audioRef.volume = event.target.value / 100;
+  handleVolume(event, muting) {
+    if (event) {
+      this.setState({volume: event.target.value});
+      this.audioRef.volume = event.target.value / 100;
+    } else if (muting) {
+      this.audioRef.muted = true;
+      this.setState({volumePreMute: this.state.volume, volume: "0"})
+    } else  {
+      this.setState({volume: this.state.volumePreMute})
+      this.audioRef.muted = false;
+      this.audioRef.volume = this.state.volumePreMute / 100;
+    }
   }
 
-  handleSeekDown(event) {
-    clearInterval(this.seekingInterval);
+
+  handleMute(event) {
+    if (!this.state.muted) {
+      this.handleVolume(null, true);
+    } else {
+      this.handleVolume(null, false);
+    }
+    this.setState({muted: !this.state.muted});
   }
 
   render() {
@@ -71,16 +93,17 @@ export class ReactAudioWrapper extends Component {
           <div
             id="play"
             onClick={this.handlePlay}>
-            Play
+            <img src="/play.png"/>
           </div>
           <div
             id="pause"
             onClick={this.handlePause}>
-            Pause
+            <img src="/pause.png"/>
           </div>
           <div
-            id="volume">
-            Volume
+            id="volume"
+            onClick={this.handleMute}>
+            <img src={this.state.muted ? "/mute.png" : "/volume.png"}/>
           </div>
           <input
             className="slider"
@@ -101,7 +124,6 @@ export class ReactAudioWrapper extends Component {
             step="1"
             value={this.state.seekerVal}
             onChange={this.handleSeekSlider}
-            onMouseDown={this.handleSeekDown}
             onMouseUp={this.handleSeek}
           />
         </div>
